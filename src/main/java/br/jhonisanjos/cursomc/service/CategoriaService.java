03b2bc72999ecec5064a9a8ace342267d4678821
Jhonis Santos dos Anjos <jhonis_10@hotmail.com.br>
2019-10-15 11:14:21 -0300

    Correções: update em CategoriaService e construtor de Pagamento

--- src/main/java/br/jhonisanjos/cursomc/service/CategoriaService.java
+++ src/main/java/br/jhonisanjos/cursomc/service/CategoriaService.java
@@ -35,10 +35,12 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	
+
 	public void delete(Integer id) {
 		this.find(id);
 		try {
@@ -59,5 +61,9 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO obj) {
 		return new Categoria(obj.getId(), obj.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
